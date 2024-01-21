7efeee6ac097213b74f8973b8038ba1b3176d4eb
Your Name <Sanchit Pandey>
2024-01-21 20:42:18 +0545

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
   },
   plugins: [require("daisyui")],
 }
+
 
