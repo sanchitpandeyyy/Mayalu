d5186198370e108e4d8e04d15df23f1dbe98991b
Your Name <Sanchit Pandey>
2024-01-21 21:21:02 +0545

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,51 @@
 import React from 'react'
+import Logo from '../../assets/logo1.png'
+import { MdLocalPhone } from 'react-icons/md';
+import { FaLocationDot } from 'react-icons/fa6';
+import { MdEmail } from "react-icons/md";
+
 
 const Footer = () => {
   return (
     <div>
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer  p-10 text-[#EAEBED] bg-[#6737c1]">
   <aside>
-    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current"><path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path></svg>
-    <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+    <img src={Logo} alt="" className='w-20' />
+    <p>Mayalu Driving, Tours and Travels<br/>Providing reliable sevices since 2067</p>
   </aside> 
-  <nav>
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
+
   <nav>
     <header className="footer-title">Company</header> 
     <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
+    <a className="link link-hover">Packages</a>
+    <a className="link link-hover">Gallery</a>
   </nav> 
   <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
+    <header className="footer-title">Get In Contact</header> 
+    <ul className="flex w-[13rem] justify-between">
+    <li>
+          <MdLocalPhone className=' w-5 h-5' />
+        </li>
+        <a href='tel:+9841148149'>9841148149</a>
+        <li>|</li>
+        <a href='tel:+9847092212'>9847092212</a>
+    </ul>
+    <ul className='flex w-[10.5rem] justify-between items-center'>
+        <li>
+          <FaLocationDot className=' h-[1rem]' />
+        </li>
+        <a target='_blank' href='https://maps.app.goo.gl/wMtvogyU9AULmPk38'>
+          Butwal, Milanchowk
+        </a>
+      </ul>
+      <ul className='flex w-[15rem] justify-between items-center'>
+        <li>
+          <MdEmail className=' h-[1rem] w-4' />
+        </li>
+        <a target='_blank' href='mailto:sanchitpandeyyy@gmail.com'>
+          sanchitpandeyyy@gmail.com
+        </a>
+      </ul>
   </nav>
 </footer>
     </div>
