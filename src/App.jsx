365d9fab340b6805dac2f9bd61398cfdedaaa1bf
Your Name <Sanchit Pandey>
2024-01-21 16:47:54 +0545

    header

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './index.scss'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+     <>
+      
+      <Header/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.scss'
+import { MdLocalPhone } from "react-icons/md";
+import { FaLocationDot } from "react-icons/fa6";
+
+const Header = () => {
+  return (
+    <div className='main-container'>
+      
+      <p className=''>Sick of the pedestrian life? Shift gears and learn to drive!</p>
+      
+      <ul className='phone'>
+        <li><MdLocalPhone /></li>
+        <a href="tel:+9841148149">9841148149</a>
+        <li>|</li>
+        <a href="tel:+9847092212">9847092212</a>
+        </ul>
+
+        <ul className='location'>
+        <li><FaLocationDot /></li>
+        <a target='_blank' href="https://maps.app.goo.gl/wMtvogyU9AULmPk38">Butwal, Milanchowk</a>
+  
+        </ul>
+        
+
+
+
+ 
+    </div>
+  )
+}
+
+export default Header
