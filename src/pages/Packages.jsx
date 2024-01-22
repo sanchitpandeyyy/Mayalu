eba7996534af0f5e795f2c828658c342ad6b711f
Your Name <Sanchit Pandey>
2024-01-23 01:57:24 +0545

    card

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -1,69 +1,63 @@
 import React from "react";
-import slider2 from '../assets/image2.jpg'
-import slider3 from '../assets/imag3.jpg'
+import car from '../assets/car.jpg';
+import bike from '../assets/bike.png';
+import scooter from '../assets/scooter.jpg'
+import slider3 from "../assets/imag3.jpg";
+import Popup from "../components/Popup/Popup";
 
 const Packages = () => {
   return (
-    <div id="packages" className="p-20 bg-[#ffffff]">
-      <h1 className="lg:text-5xl text-4xl font-bold text-center">Our Packages</h1>
+    <div id="packages" className="md:p-20 pt-20 bg-[#ffffff]">
+      <h1 className="lg:text-5xl text-4xl font-bold text-center">
+        Our Packages
+      </h1>
 
       <div className="p-8 flex flex-col justify-between lg:flex-row items-center">
-      <div className="card max-w-[40vw] max-h-[15rem] overflow-hidden mt-10 lg:w-80 bg-base-100 shadow-xl image-full ">
-          <figure>
-      
-            <img
-              src={slider3}
-              alt="Car"
-              className="w-full"
-            />
-          </figure>
-          <div className="card-body  hover:shadow-2xl rounded-xl">
-            <h2 className="card-title">Car</h2>
-            <p className="text-[0.7rem] md:w-auto">Learn to Drive Car with our Experts!</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary md:w-auto w-[25vw] h-auto flex items-center justify-center">View More</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="card mt-10 max-w-[40vw] max-h-[15rem] overflow-hidden lg:w-80 bg-base-100 shadow-xl image-full">
-          <figure>
-            <img
-              src={slider3}
-              alt="Bike"
-              className="w-full"
-            />
-          </figure>
-          <div className="card-body hover:shadow-2xl rounded-xl">
-            <h2 className="card-title">Bike</h2>
-            <p className="text-[0.7rem] md:w-auto">Learn to Drive Bike with our Experts!</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary md:w-auto w-[25vw] h-auto flex items-center justify-center">View More</button>
-            </div>
-          </div>
-        </div>
-
-        <div className="card max-w-[40vw] max-h-[15rem] overflow-hidden mt-10  lg:w-80 bg-base-100 shadow-xl image-full">
-          <figure>
-            <img
-              src={slider3}
-              alt="Scooter"
-              className="w-full"
-            />
-          </figure>
-          <div className="card-body hover:shadow-2xl rounded-xl">
-            <h2 className="card-title">Scooter</h2>
-            <p className="text-[0.7rem] md:w-auto">Learn to Drive Bike with our Experts!</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary md:w-auto w-[25vw] h-auto flex items-center justify-center ">View More</button>
-            </div>
-          </div>
-        </div>
-
         
+        <div className="card max-w-[50vw] max-h-[25rem] overflow-hidden m-10 text-black lg:w-80 bg-[#f5f5f5] shadow-xl">
+          <figure>
+            <img src={car} className="w-full" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title lg:text-lg text-md">Car</h2>
+            <p className="lg:text-md text-sm">
+              Learn to Drive Car with our Experts!
+            </p>
+            <div className="card-actions justify-end">
+              <Popup />
+            </div>
+          </div>
+        </div>
 
-        
+        <div className="card max-w-[50vw] max-h-[25rem] overflow-hidden m-10 text-black lg:w-80 bg-[#f5f5f5] shadow-xl">
+          <figure>
+            <img src={bike} className="w-full" alt="Bike" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title lg:text-lg text-md">Bike</h2>
+            <p className="lg:text-md text-sm">
+              Learn to Drive Bike with our Experts!
+            </p>
+            <div className="card-actions justify-end">
+              <Popup />
+            </div>
+          </div>
+        </div>
 
+        <div className="card max-w-[50vw] max-h-[25rem] overflow-hidden m-10 text-black lg:w-80 bg-[#f5f5f5] shadow-xl">
+          <figure>
+            <img src={scooter} className="w-full" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title lg:text-lg text-md">Scooter</h2>
+            <p className="lg:text-md text-sm">
+              Learn to Drive Scooter with our Experts!
+            </p>
+            <div className="card-actions justify-end">
+              <Popup />
+            </div>
+          </div>
+        </div>
 
       </div>
     </div>
